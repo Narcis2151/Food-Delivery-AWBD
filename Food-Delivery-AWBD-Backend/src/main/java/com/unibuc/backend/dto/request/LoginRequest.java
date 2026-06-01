0bbf9711eefcb681ea9437a63bf386475e1981dd
Narcis Fanica <narcis.fanica@vivacredit.ro>
2026-06-01 13:23:49 +0300

    Implement Basic backend jwt authentication and role based authorization

---
+++ Food-Delivery-AWBD-Backend/src/main/java/com/unibuc/backend/dto/request/LoginRequest.java
@@ -0,0 +1,31 @@
+package com.unibuc.backend.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Schema(description = "Login Details")
+public class LoginRequest {
+
+    @NotBlank()
+    @Email()
+    @Schema(description = "Email Of The User", example = "test.user@mail.com")
+    private String email;
+
+    @NotBlank()
+    @Schema(description = "Password Of The User", example = "password")
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
---
+++ Food-Delivery-AWBD-Backend/src/main/java/com/unibuc/backend/model/ERole.java
@@ -0,0 +1,7 @@
+package com.unibuc.backend.model;
+
+public enum ERole {
+    ROLE_CUSTOMER,
+    ROLE_STORE_OWNER,
+    ROLE_ADMIN
+}
